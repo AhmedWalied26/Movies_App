f6a1cbfb9bed1e860d465c82b8d99f15998abdbf
Ahmed Walied <2301647@student.eelu.edu.eg>
2026-08-13 19:04:37 +0300

    feat(search): implement search tab UI

--- lib/features/main/tabs/search/search_tab.dart
+++ lib/features/main/tabs/search/search_tab.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:movies_app/l10n/app_localizations.dart';
+import 'package:movies_app/utils/app_assets.dart';
+import 'package:movies_app/utils/size_utils.dart';
+import 'package:movies_app/widgets/custom_text_field.dart';
 
 class SearchTab extends StatelessWidget {
   const SearchTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var width = context.width;
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: width * 0.035),
+        child: Column(
+          children: [
+            CustomTextField(
+              title: AppLocalizations.of(context)!.search,
+              prefix: SvgPicture.asset(AppAssets.searchIcon),
+            ),
+            Expanded(
+              child: Center(child: Image.asset(AppAssets.emptyListImage)),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
