771e1d28956136cf8853966f4800410338a89c57
Ahmed Walied <2301647@student.eelu.edu.eg>
2026-09-02 11:11:00 +0300

    feat(search): update search tab

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,7 @@ class MoviesApp extends StatelessWidget {
         AppRoutes.browseScreen: (context) => BrowseTab(),
         AppRoutes.profileScreen: (context) => ProfileTab(),
       },
-      initialRoute: AppRoutes.exploreScreen,
+      initialRoute: AppRoutes.mainScreen,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.dark,
